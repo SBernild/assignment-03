5f10bbf3522a4c6baffd3829bde63f2bbba7dab9
Frederik-Hoegh-Christensen <frederik.hoegh@gmail.com>
2022-09-27 13:28:34 +0200

    added a kabancontext whith no red underlines

--- Assignment.Infrastructure/KanbanContext.cs
+++ Assignment.Infrastructure/KanbanContext.cs
@@ -1,5 +1,59 @@
+
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+
 namespace Assignment.Infrastructure;
 
-public class KanbanContext
-{
-}
+
+
+
+    public partial class KabanContext : DbContext{
+        public KabanContext(DbContextOptions<KabanContext> options) : base(options)
+        {
+            
+        }
+
+        public virtual DbSet<Tag> Tags {get; set;} =null!;
+        public virtual DbSet<WorkItem> Tasks {get; set;} =null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Tag>(entity => {
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<WorkItem>(entity => {
+                entity.Property(e => e.Title);
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+    internal class KabanContextFactory : IDesignTimeDbContextFactory<KabanContext>
+    {
+        KabanContext IDesignTimeDbContextFactory<KabanContext>.CreateDbContext(string[] args){
+        var configuration = new ConfigurationBuilder().AddUserSecrets<KabanContext>().Build();
+        var connectionString = configuration.GetConnectionString("Kaban");
+
+        var optionsBuilder = new DbContextOptionsBuilder<KabanContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new KabanContext(optionsBuilder.Options);
+        }
+    }
+
+    
+
+    
+
+
+
+
+
+
+
+
