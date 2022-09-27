0e7294219d774867ffa690193637b763e0cf4d01
Megabd <bath@itu.dk>
2022-09-27 12:55:47 +0200

    Constructors

--- Assignment.Infrastructure/User.cs
+++ Assignment.Infrastructure/User.cs
@@ -9,4 +9,12 @@ public class User
     [StringLength(100), Required]
     public string Email { get; set; }
     public IList<WorkItem> WorkItems { get; set; }
+
+    public User (int Id, string Name, string Email, IList<WorkItem> WorkItems){
+
+        this.Id = Id;
+        this.Name = Name;
+        this.Email = Email;
+        this.WorkItems = WorkItems;
+    }
 }
