ef291c8a8a5e0fd55955d09b9ce642e8703706ac
Frederik-Hoegh-Christensen <frederik.hoegh@gmail.com>
2022-09-29 13:27:25 +0200

    Tests are now possible

--- Assignment.Infrastructure.Tests/TagRepositoryTests.cs
+++ Assignment.Infrastructure.Tests/TagRepositoryTests.cs
@@ -1,6 +1,43 @@
 namespace Assignment.Infrastructure.Tests;
+using Assignment.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Assignment.Core;
+using Microsoft.Data.Sqlite;
 
-public class TagRepositoryTests
+public class TagRepositoryTests : IDisposable
 {
-    
+    private KabanContext _context;
+    public TagRepository _repo;
+
+    public TagRepositoryTests(){
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<KabanContext>();
+        builder.UseSqlite(connection);
+        var context = new KabanContext(builder.Options);
+        context.Database.EnsureCreated();
+        context.Tags.Add(new Tag{Id = 1, Name = "frederik"});
+        context.SaveChanges();
+
+        _context = context;
+        _repo = new TagRepository(_context);
+    }
+
+
+
+    [Fact]
+    public void find_none_existing_tag()
+    {
+        // Given
+        var (response, created) = _repo.Create(new TagCreateDTO("yo"));
+        // When
+        response.Should().Be(Response.Created);
+        created.Should().Be(2);
+        // Then
+    }
+
+    void IDisposable.Dispose()
+    {
+        _context.Dispose();
+    }
 }
---
+++ Assignment.Infrastructure.Tests/TestConnection.cs
@@ -0,0 +1,28 @@
+using Assignment.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using Microsoft.Extensions.Configuration;
+
+public abstract class TestConnection{
+        private DbContext _context;
+        public TestConnection(){
+            /* DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder<KabanContext>();
+            optionsBuilder.UseSqlServer("Server=localhost; Database=Master; User Id=SA; Awes0mepassword!; Trusted_Connection=False;Encrypt=False");
+            DbContextOptions options = optionsBuilder.Options;
+
+            _context = new KabanContext(options); */
+            var optionsBuilder = new DbContextOptionsBuilder<KabanContext>();
+            
+            optionsBuilder.UseSqlServer("Server=localhost; Database=Master; User Id=SA; Password=Awes0mepassword!; Encrypt=False");
+            
+            var options = optionsBuilder.Options;
+            
+
+             _context = new KabanContext(options);
+            
+        }
+ 
+}
+
